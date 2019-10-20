abe47d81437881b14d3e8cc7206fe5bba76fa969
LiudmilaVyatkina <lvtk2015@gmail.com>
2019-10-20 15:22:51 -0400

    Day4 code

--- src/test/java/GoogleSearchTests.java
+++ src/test/java/GoogleSearchTests.java
@@ -1,3 +1,78 @@
-public class GoogleSearchTests {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
+public class GoogleSearchTests<waitForResultsElement> {
+
+    WebDriver driver;
+    String resultStatsID = "resultStats";
+
+    @AfterSuite
+    public void AfterSuite() {
+     //   driver.quit();
+    }
+
+    @Test
+    public void test0001() {
+        System.setProperty("webdriver.gecko.driver", "/Users/admin/IdeaProjects/sepJavaTestNG/src/test/resources/drivers/geckodriver");
+
+
+    openBrowser();
+    novigateToMainPage();
+    typeQuory();
+    submitSerach();
+    waitForResultsElement();
+    assertResultsPage();
+    }
+
+     private void waitForResultsElement(){
+
+        By elementToWait = By.id(resultStatsID);
+        ExpectedCondition condition = ExpectedConditions.presenceOfElementLocated(elementToWait);
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
+        webDriverWait.until(condition);
+
+    }
+    private void assertResultsPage() {
+
+        WebElement resultStatsElement = driver.findElement(By.id(resultStatsID));
+
+        boolean isDisplayed = resultStatsElement.isDisplayed();
+        boolean expected = true;
+
+        Assert.assertEquals(isDisplayed, expected);
+
+    }
+
+    private void submitSerach() {
+        String selector = ".gLFyf";
+        driver.findElement(By.cssSelector(selector)).submit();
+    }
+
+    private void typeQuory() {
+        String selector = ".gLFyf";
+        WebElement webElement = driver.findElement(By.cssSelector(selector));
+        webElement.sendKeys( "Portnov Computer School");
+
+    }
+
+    private void novigateToMainPage() {
+        String url = "http://www.google.com";
+        driver.get(url);
+
+    }
+
+    private void openBrowser() {
+        driver = new FirefoxDriver();
+      //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 }
