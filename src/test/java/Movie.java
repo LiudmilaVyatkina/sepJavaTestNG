8a1d091ff46d7d367289830f21a0eaa20115444a
LiudmilaVyatkina <lvtk2015@gmail.com>
2019-11-25 20:12:54 -0500

    Day 6 code

---
+++ src/test/java/Movie.java
@@ -0,0 +1,23 @@
+public class Movie {
+
+    String name;
+    String director;
+    float rating;
+    private boolean released;
+
+    public Movie(String name, String director, float rating) {
+        this.name = name;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    public void  release() {
+        released = true;
+    }
+    boolean isReleased(){
+        return  released;
+    }
+    public void watch(){
+        System.out.println("Watching " + name + " by " + director + " with rating " + rating);
+    }
+}
