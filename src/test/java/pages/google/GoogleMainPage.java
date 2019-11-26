8dd56e802e13ee77092635adc58e11259c4c002b
LiudmilaVyatkina <lvtk2015@gmail.com>
2019-11-25 20:08:44 -0500

    Day 6 code

--- src/test/java/pages/google/GoogleMainPage.java
+++ src/test/java/pages/google/GoogleMainPage.java
@@ -5,39 +5,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class GoogleMainPage extends BaseTest {
 
-    WebDriver driver;
-    By textInputByCss = By.cssSelector("#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input");
 
-    String url = "http://www.google.com";
+    public class GoogleMainPage extends BaseTest {
 
-    public GoogleMainPage(WebDriver driver) {
-        this.driver = driver;
+        WebDriver driver;
+        By textInputByCss = By.cssSelector("#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input");
+
+        String url = "http://www.google.com";
+
+        public GoogleMainPage(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        public void novigate() {
+            driver.get(url);
+        }
+
+
+        public void typeQuery(String quoryToType) {
+            WebElement webElement = driver.findElement(textInputByCss);
+            webElement.sendKeys(quoryToType);
+        }
+
+        public void submitSerach() {
+
+            driver.findElement(textInputByCss).submit();
+        }
+
+
+        public void verifySuggestion() {
+            // driver.findElement(textInputByCss).getAttribute();
+        }
+
+
+
+
     }
 
-    public void novigate() {
-        driver.get(url);
-    }
 
-
-    public void typeQuery(String quoryToType) {
-        WebElement webElement = driver.findElement(textInputByCss);
-        webElement.sendKeys(quoryToType);
-    }
-
-    public void submitSerach() {
-
-        driver.findElement(textInputByCss).submit();
-    }
-
-
-    public void verifySuggestion() {
-       // driver.findElement(textInputByCss).getAttribute();
-    }
-
-
-
-
-}
 
