6faf64a85190da1ce172981106be13cd353094d1
LiudmilaVyatkina <lvtk2015@gmail.com>
2019-11-25 20:09:32 -0500

    Day 6 code

--- src/test/java/googlesearchuitets/BaseTest.java
+++ src/test/java/googlesearchuitets/BaseTest.java
@@ -7,20 +7,25 @@ import org.testng.annotations.BeforeSuite;
 
 import java.util.concurrent.TimeUnit;
 
-public class BaseTest {
-    WebDriver driver;
-
-    @BeforeSuite
-
-    public void setupBrowser() {
 
 
-        System.setProperty("webdriver.gecko.driver", "/Users/admin/IdeaProjects/sepJavaTestNG/src/test/resources/drivers/geckodriver");
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public class BaseTest {
+        WebDriver driver;
+
+        @BeforeSuite
+
+        public void setupBrowser() {
+
+
+            System.setProperty("webdriver.gecko.driver", "/Users/admin/IdeaProjects/sepJavaTestNG/src/test/resources/drivers/geckodriver");
+            driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        }
+
+        @AfterSuite
+        public void tearDown() {
+            driver.quit();
+        }
     }
 
-    @AfterSuite
-    public void tearDown() { driver.quit(); }
 
-}
